4dbf41fb852c79e32f4fc5b39048227c7729c729
GuillerminaS <guillerminasalinas.ok@gmail.com>
2022-09-23 11:10:03 -0300

    Add: base de Api backend con SpringBoot

---
+++ PortfolioAP/src/main/java/com/guillerminaSalinas/PortfolioAP/PortfolioApApplication.java
@@ -0,0 +1,13 @@
+package com.guillerminaSalinas.PortfolioAP;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApApplication.class, args);
+	}
+
+}
