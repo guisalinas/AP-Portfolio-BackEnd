da243afc6c2796d489ecae161082c5a212e21bf1
GuillerminaS <guillerminasalinas.ok@gmail.com>
2022-09-23 23:12:32 -0300

    DB: conexión exitosa. Modelo Person de prueba

--- PortfolioAP/src/main/java/com/guillerminaSalinas/PortfolioAP/Controller/Controller.java
+++ PortfolioAP/src/main/java/com/guillerminaSalinas/PortfolioAP/Controller/Controller.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.guillerminaSalinas.PortfolioAP.Controller;
 
-/**
- *
- * @author GuiSalinas
- */
+import com.guillerminaSalinas.PortfolioAP.Model.Person;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+    
+    List<Person> personList = new ArrayList();
+    
+    //crear persona
+    @PostMapping("/new/person")
+    public void addPerson(@RequestBody Person p){
+        personList.add(p);
+    }
+    
+    //mostrar lista de personas
+    @GetMapping("/get/person")
+    @ResponseBody
+    public List<Person> getPersonList(){
+        return personList;
+    }
+            
     
 }
---
+++ PortfolioAP/src/main/java/com/guillerminaSalinas/PortfolioAP/Model/Person.java
@@ -0,0 +1,40 @@
+package com.guillerminaSalinas.PortfolioAP.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Person {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String surname;
+    private String about_me;
+    private String email;
+    private String photo_url;
+    private String description;  
+    
+    public Person (){}
+    
+    public Person(int id, String name, String surname, String about_me, String email, String photo_url, String description){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.about_me = about_me;
+        this.email = email;
+        this.photo_url = photo_url;
+        this.description = description;
+    }   
+    
+}
+
+
+
+
